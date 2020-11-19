00c781c6a3394d2085a128750963525153fd36a4
leolanese <developer@leolanese.com>
2020-11-19 22:46:56 +0000

    unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import {async, TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
 import {RouterTestingModule} from '@angular/router/testing';
 
-describe('AppComponent', () => {
+describe('Validate AppComponent', () => {
   const defaultLanguageMock = 'en';
 
   beforeEach(async(() => {
